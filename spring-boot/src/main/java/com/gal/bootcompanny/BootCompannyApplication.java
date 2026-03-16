275e45716d0c21c6425f798bad99e1ddc5082f3a
Unnati Bhardwaj <unnatibhardwaj1729@gmail.com>
2026-03-17 00:40:30 +0530

    added assesmen spring folder

--- spring-boot/src/main/java/com/gal/bootcompanny/BootCompannyApplication.java
+++ spring-boot/src/main/java/com/gal/bootcompanny/BootCompannyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootCompannyApplication {
+public class        BootCompannyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootCompannyApplication.class, args);
