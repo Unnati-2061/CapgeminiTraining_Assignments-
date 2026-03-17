03bdb98133b46ca0f4ba3b0af197c1db0f38e94e
Unnati Bhardwaj <unnatibhardwaj1729@gmail.com>
2026-03-17 12:05:56 +0530

    add

--- spring-boot/src/main/java/com/gal/bootcompanny/BootCompannyApplication.java
+++ spring-boot/src/main/java/com/gal/bootcompanny/BootCompannyApplication.java
@@ -1,13 +1,22 @@
 package com.gal.bootcompanny;
 
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class        BootCompannyApplication {
-
+public class BootCompannyApplication {
+//    private static LoggerContext loggerFactory;
+    public static final Logger log = LoggerFactory.getLogger(BootCompannyApplication.class);
     public static void main(String[] args) {
+
         SpringApplication.run(BootCompannyApplication.class, args);
+        ConfigurableApplicationContext context  = SpringApplication.run(BootCompannyApplication.class);
+
+        log.info("Application started successfully");
     }
 
 }
